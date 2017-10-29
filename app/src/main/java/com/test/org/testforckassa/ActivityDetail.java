982ad0e86d416f41413b5ba8f5b90a4a50eed026
LegDK <maltsev.ru95@mail.ru>
2017-10-30 01:09:55 +0500

    1

--- app/src/main/java/com/test/org/testforckassa/ActivityDetail.java
+++ app/src/main/java/com/test/org/testforckassa/ActivityDetail.java
@@ -1,18 +1,37 @@
 package com.test.org.testforckassa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class ActivityDetail extends AppCompatActivity {
     private EditText editTextHead;
     private EditText editTextDescription;
+    private Button buttonSave;
+    private Button buttonDelete;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         editTextHead = (EditText) findViewById(R.id.editHead);
         editTextDescription = (EditText) findViewById(R.id.editDescription);
+        buttonSave = (Button) findViewById(R.id.buttonSave);
+        buttonDelete = (Button) findViewById(R.id.buttonDelete);
+        Intent intent = getIntent();
+        String access = intent.getStringExtra("Access");
+        switch (access){
+            case "Read":
+                editTextHead.setInputType(InputType.TYPE_NULL);
+                editTextDescription.setInputType(InputType.TYPE_CLASS_TEXT);
+                break;
+            case "Write":
+                editTextHead.setInputType(InputType.TYPE_CLASS_TEXT);
+                editTextDescription.setInputType(InputType.TYPE_CLASS_TEXT);
+                break;
+        }
 
     }
 }
--- app/src/main/java/com/test/org/testforckassa/MainActivity.java
+++ app/src/main/java/com/test/org/testforckassa/MainActivity.java
@@ -3,19 +3,37 @@ package com.test.org.testforckassa;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.test.org.testforckassa.models.ListItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    DBHelp dbHelp;
+    private DBHelp dbHelp;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private List<ListItem> listItems;
+    private LinearLayoutManager linearLayoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         dbHelp=new DBHelp(this);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler);
+        listItems = new ArrayList<>();
+        recyclerView.setHasFixedSize(true);
+        linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        adapter = new ViewAdapter(listItems,getApplicationContext());
+
     }
     @Override
     public boolean onCreateOptionsMenu (Menu menu)
@@ -30,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
         super.onOptionsItemSelected(item);
         if (item.getItemId()==R.id.icon_plus){
             Intent intent = new Intent(this,ActivityDetail.class);
+            intent.putExtra("Access","Read");
             startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
